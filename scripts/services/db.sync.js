7e945322a5b0d841782690eebdf7d9735ceb6af9
lict@novasoftware.cn <lict@novasoftware.cn>
2015-06-19 10:20:34 +0800

    --------

--- scripts/services/db.sync.js
+++ scripts/services/db.sync.js
@@ -107,9 +107,21 @@ AKHB.services.db.DBSync =  (function(){
 						},
 						function(url,callback){
 							console.log("syncMessage 2",url);
-							$.getJSON(url,function(result){
-								callback(null,result);
-							});
+							jQuery.ajax({
+			                    url: url,
+			                    type: "get",
+			                    dataType: "json",
+			                    success: function(msg) {
+			                        callback(null,result);
+			                    },
+			                    error: function(XMLHttpRequest, textStatus, errorThrown) {
+			                    	console.log(errorThrown);
+			                        callback(null,null);
+			                    },
+			                    complete: function(XMLHttpRequest, textStatus) {
+			                        //this; // 调用本次AJAX请求时传递的options参数
+			                    }
+			                });
 						},
 						function(result,callback){
 							console.log("syncMessage 3");
